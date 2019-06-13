b2a0172a4d8376ebcdf3efcaab424968aa5178f2
Kristin Kraemer <kristinhannahk@gmail.com>
2019-06-13 21:17:44 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -79,7 +79,7 @@ class Owner
   
   def list_pets 
     
-    return "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dogs(s), and #{@pets[:cats].length} cat(s)."
+    return "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end 
   
 end
