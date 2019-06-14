286ffc8d519a221c1c23c7b3d6548f49494f20a7
Alex Velasquez <jtinnesco@gmail.com>
2019-06-14 23:50:56 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
-class Dog
-  # code goes here
+class Dog 
+  attr_accessor :mood 
+  attr_reader :name 
+  
+  def initialize(name, mood = "nervous")
+    @name = name 
+    @mood = mood
+  end
+  
+  
 end
